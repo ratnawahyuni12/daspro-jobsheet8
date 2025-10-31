522112ad6c99fb4107ded4717f057a9a61eaf401
ratnawahyuni12 <ratna120307@gmail.com>
2025-10-31 14:21:23 +0700

    Memperbaiki Percobaan 3 : Bintang Segitiga

--- Triangle21.java
+++ Triangle21.java
@@ -4,13 +4,14 @@ public class Triangle21 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
-        int i = 0;
+        int i = 1;
         while (i <= N) {
-            int j = 0;
-            while (j < i) {
+            int j = 1;
+            while (j <= i) {
                 System.out.print("*");
                 j++;
             }
+            System.out.println();
             i++;
         }
     }
